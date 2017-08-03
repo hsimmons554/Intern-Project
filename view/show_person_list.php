e75e64dd225698142f817eb919af19dd6f667e80
Harold <hsimmons554@gmail.com>
2017-08-03 16:10:17 -0500

    taking break after failing a lot

--- view/show_person_list.php
+++ view/show_person_list.php
@@ -10,15 +10,27 @@
 </form>
 <form ".">
   <input type="hidden" name="action" value="show_person_stats" />
-  <select name="person_id">
+<div id="person_list">
+  <select id="person_id" name="person_id" pagecontainerload="loadPeople(this.value)" /><!--
+    <script type="text/javascript" charset="utf-8">
+      $.getJSON("api/people", function(json){
+        $('#select').empty();
+        $('#select').append($('<option>').text("Select"));
+        $.each(json, function(i, obj){
+          $('#select').append($('<option>').text(obj.first_name).attr('value', obj.id));
+        });
+      });
+    </script> -->
+    <!-- <?php /*
     <?php foreach($persons as $person) : ?>
     <option value="<?php echo $person['id']; ?>"
       <?php if($person['id'] == $person_stats['id']) : ?>
         selected <?php endif; ?>>
       <?php echo $person[first_name] . ' ' . $person[last_name]; ?>
     </option>
-    <?php endforeach; ?>
+  <?php endforeach; ?> */ ?>-->
   </select>
+</div>
   <input type="submit" value="Submit Person" />
 </form><br>
 <?php if($flag) : ?>
@@ -33,3 +45,34 @@
     <?php {echo '<label>States: None</label>'; break;} ?>
   <?php endif; ?>
 <?php endif; ?>
+<script>
+  function loadPeople(str) {
+    $.ajax({
+      url: 'api/people',
+      type: 'GET',
+      data: 'q=' + str,
+      dataType: 'json',
+      success: function( json ) {
+        $.each(json, function(i, value) {
+          $('#person_list').append($('<option>').text(value).attr('value', value));
+        });
+      }
+    });
+  };
+</script>
+<!--
+<script>
+  function loadPeople() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        <! Code to load list of people >
+        document.getElementById("person_list").innerHTML =
+        this.responseText;
+      }
+    };
+    xhttp.open("GET", "api/people", true);
+    xhttp.send();
+  }
+</script> -->
+<?php include('footer.php'); ?>
